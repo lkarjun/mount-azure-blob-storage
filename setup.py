c076b6661a74269215b90e631def20adf0d370b4
LalKrishna Arjun <58617251+lkarjun@users.noreply.github.com>
2023-03-01 09:05:36 +0000

    Add long description

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setuptools.setup(
     author = "Lalkrishna",
     url = "arju66938@gmail.com",
     description = "Mount Azure blob storage in google colab.",
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
     py_modules=['mount_azure_blob'],
     install_requires = "ipywidgets",
     python_requires = ">=3.7",
