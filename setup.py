4dc1165e97eb0f54259cbb256ae0f0befdad5393
LalKrishna Arjun <58617251+lkarjun@users.noreply.github.com>
2023-03-01 11:50:39 +0000

    Requires python-dotenv

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
     py_modules=['mount_azure_blob'],
-    install_requires = "ipywidgets",
+    install_requires = ["ipywidgets", "python-dotenv"],
     python_requires = ">=3.7",
     zip_safe = False,
 )
