7b46fa9e7c53ebe65864997a236485414a92251f
LalKrishna Arjun <58617251+lkarjun@users.noreply.github.com>
2023-03-01 18:29:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "mount_azure_blob",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Lalkrishna",
     url = "https://github.com/lkarjun/mount-azure-blob-storage/",
     description = "Mount Azure blob storage in google colab.",
